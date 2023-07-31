fbb4ccec1d80557f432252657ea4e9e5163a44d0
Damian <errollgarner@yandex.ru>
2023-07-31 13:27:31 +0200

    rollback and fix
    [need build client]
    [need deploy server]

--- api/services/dbConnect.js
+++ api/services/dbConnect.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 async function dbConnect() {
     if (process.env.NODE_ENV === "development "){
         mongoUrl = "mongodb://qwerty:" + process.env.MONGO_DEV_PASS + "@localhost:27017/behindline?authSource=behindline"
-    } else {
         console.log(process.env.NODE_ENV + '1');
+    } else {
+        console.log(process.env.NODE_ENV + '2');
         mongoUrl = "mongodb://qwerty:" + process.env.MONGO_PROD_PASS + "@127.0.0.1:27017/behindline?authSource=behindline"
     }
 
